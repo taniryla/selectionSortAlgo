554ab5677cb7970f5b787603c0db3c993643b0ee
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-31 18:45:35 -0700

    nested for loop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {}
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {}
+  }
 }
